07ce5883163b6b0378ae0b68bf6dcb624b5bf681
Biswajit Biswas <bbiswas@linkedin.biz>
2021-12-31 19:54:02 +0530

    Updated

---
+++ enable_ssl/mainpol.ps1
@@ -0,0 +1,35 @@
+[void](Import-Module Polaris)
+Set-Location C:\WebApi
+#$VerbosePreference = 'Continue' # Comment this to stop transcript generation
+$dateformat = Get-Date -format 'MM.dd.yyyy.HH.mm.ss' # modified this to reflect the exact time in 24 hour format
+#$Subdateformat = Get-Date -format 'MM.dd.yyyy'
+$LoggingDirectory = "C:\WebApi\Logs"
+Start-Transcript -Path "$($LoggingDirectory)\Polaris_$($dateformat).log" -Force
+$process = Get-Process -Id $pid
+$process.PriorityClass = 'RealTime'
+#Netsh http delete sslcert ipport=0.0.0.0:443
+#Change the IP
+Invoke-Expression -Command "Netsh http delete sslcert ipport=0.0.0.0:443"
+Invoke-Expression -Command "Netsh http delete sslcert ipport=192.168.1.5:443"
+#Start-Sleep -Seconds 5
+$AppID = "{" + $(New-Guid) + "}"
+#Change the DNS Alias
+#$HostName = 'polapi.bshwjt.biz'
+$Port = '443'
+#Change IP & Certificate Thumprint
+Invoke-Expression -Command "netsh http add sslcert ipport=0.0.0.0:$($Port) certhash=b9e617a9729fbac954f54a2c8ed58eec1aae3151 appid='$($AppID)' certstorename=MY"
+Invoke-Expression -Command "netsh http add sslcert ipport=192.168.1.5:$($Port) certhash=b9e617a9729fbac954f54a2c8ed58eec1aae3151 appid='$($AppID)' certstorename=MY"
+Set-Location C:\WebApi
+#REGION GET ROUTES
+.\routes\home.ps1
+.\routes\certexpiry.ps1
+.\routes\employees.ps1
+#ENDREGION
+Start-Polaris -Port $Port -MaxRunspaces 5 -Debug -Verbose -Https -Auth IntegratedWindowsAuthentication
+
+while ($true) {
+    Start-Sleep -Milliseconds 10
+}
+Stop-Transcript
+
+
