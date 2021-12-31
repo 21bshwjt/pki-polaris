53ae13f43226340e28a3c8ed670551c572186a36
Biswajit Biswas <42672713+21bshwjt@users.noreply.github.com>
2021-12-31 06:09:50 +0000

    Add files via upload

---
+++ subroutes/employees.ps1
@@ -0,0 +1,14 @@
+﻿[void](Import-Module Pswritehtml -Force)
+$getempdata = Get-Content C:\WebApi\apicache\employees.json | ConvertFrom-Json
+
+New-HTML -TitleText "Identity" {
+    New-HTMLSection -HeaderText '<h1>Contoso</h1>' {
+        New-HTMLContent -HeaderText "<h3>Team Info</h3>" -CanCollapse -HeaderTextColor Black -HeaderBackGroundColor PaleGoldenrod {
+            New-HTMLTable -Title "Copper" -DataTable $getempdata -HideFooter -PagingOptions @(50, 100, 500, 1000) {
+                            
+            } 
+        }
+        
+    }
+
+}
