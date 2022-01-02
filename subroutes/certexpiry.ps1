2662e25b0c7cb5a27766a9f223de099e71220df9
Biswajit Biswas <42672713+21bshwjt@users.noreply.github.com>
2022-01-02 11:12:51 +0530

    Update certexpiry.ps1

--- subroutes/certexpiry.ps1
+++ subroutes/certexpiry.ps1
@@ -7,7 +7,7 @@ $data = Get-Content "C:\WebApi\apicache\cert.json" | ConvertFrom-Json
 $DbwriteTime = (Get-Item "C:\WebApi\apicache\cert.json").LastWriteTime
 New-HTML -FavIcon $icon -TitleText $Title -AutoRefresh 50 {
     New-HTMLContent -HeaderText "<center>$headertxt</center><span><center>Refreshed: $DbwriteTime</center></span>" {
-        New-HTMLTable -Title $TableTitle -DataTable $data -HideFooter -PagingOptions @(12, 24) {
+        New-HTMLTable -Title $TableTitle -DataTable $data -HideFooter -PagingOptions @(25, 50) {
          #Conditional Formatting
          TableConditionalFormatting -Name 'DaysUntilExpired' -ComparisonType number -Operator le -Value 364 -Color white -BackgroundColor Red
          TableConditionalFormatting -Name 'DaysUntilExpired' -ComparisonType number -Operator ge -Value 363 -Color Black -BackgroundColor PaleGreen
