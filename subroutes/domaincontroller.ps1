f1a36dec1158381d3cecd8bcd67e2f0ee8d84710
Biswajit Biswas <42672713+21bshwjt@users.noreply.github.com>
2022-01-02 07:36:27 +0000

    Add files via upload

---
+++ subroutes/domaincontroller.ps1
@@ -0,0 +1,18 @@
+﻿[void](Import-Module PSWriteHTML)   
+$Title = 'Dashboard | Domain Controllers'
+$icon = 'https://pngmind.com/wp-content/uploads/2019/08/Linkedin-Logo-Png-Transparent-Background.png'
+$headertxt = "<h1>Corp Domain Controllers</h1>"
+$TableTitle = "Corp Domain Controller"
+$Get_dcs = [system.directoryservices.activedirectory.domain]::GetCurrentDomain().DomainControllers | Select Name,IPAddress,OSVersion,SiteName
+#$DbwriteTime = (Get-Item "C:\WebApi\apicache\cert.json").LastWriteTime
+New-HTML -FavIcon $icon -TitleText $Title -AutoRefresh 50 {
+    New-HTMLContent -HeaderText "<center>$headertxt</center>" {
+        New-HTMLTable -Title $TableTitle -DataTable $Get_dcs -HideFooter -PagingOptions @(25, 50) {
+         #Conditional Formatting
+         #TableConditionalFormatting -Name 'DaysUntilExpired' -ComparisonType number -Operator le -Value 364 -Color white -BackgroundColor Red
+         #TableConditionalFormatting -Name 'DaysUntilExpired' -ComparisonType number -Operator ge -Value 363 -Color Black -BackgroundColor PaleGreen
+            
+        } 
+    }
+}
+ 
