f10534db4931333835f1481a42aac4aa62468472
Biswajit Biswas <42672713+21bshwjt@users.noreply.github.com>
2022-01-02 07:35:54 +0000

    Add files via upload

---
+++ routes/domaincontroller.ps1
@@ -0,0 +1,5 @@
+﻿New-PolarisGetRoute -Path "/domaincontroller" -Scriptblock {
+    $getdcs = . C:\WebApi\subroutes\domaincontroller.ps1
+    $Response.SetContentType("text/html")
+    $Response.Send($getdcs)
+}
