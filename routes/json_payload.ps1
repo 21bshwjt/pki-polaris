711622d3b233adef82dabde5f66bb66df5582414
Biswajit Biswas <42672713+21bshwjt@users.noreply.github.com>
2022-01-16 13:25:42 +0530

    Update json_payload.ps1

--- routes/json_payload.ps1
+++ routes/json_payload.ps1
@@ -1,4 +1,4 @@
-New-PolarisGetRoute -Path "/certmon" -Scriptblock {
+New-PolarisGetRoute -Path "/json_payload" -Scriptblock {
     $pkiexp = . C:\WebApi\subroutes\json_payload.ps1
     $Response.SetContentType( "application/json" )
     $Response.json($pkiexp)
