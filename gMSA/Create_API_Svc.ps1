0169e11457020154487091a272a97310e936415e
Biswajit Biswas <bshwjt@gmail.com>
2022-02-27 11:15:01 +0530

    Removed spaces from gMSA code

--- gMSA/Create_API_Svc.ps1
+++ gMSA/Create_API_Svc.ps1
@@ -1,19 +1,19 @@
 ﻿function Install-Service {
 
-    Param(
+    Param(
 
-        [string]$nssmPath = 'C:\nssm-2.24\win64',
-        [string]$Name,
-        [string]$Description,
-        [string]$Executable,
-        [string]$Arguments
+        [string]$nssmPath = 'C:\nssm-2.24\win64',
+        [string]$Name,
+        [string]$Description,
+        [string]$Executable,
+        [string]$Arguments
 
-    )
+    )
 
-    $nssm = Join-Path -Path $nssmPath -ChildPath 'nssm.exe'
-    & $nssm install $name $executable $arguments
-    $null = & $nssm set $name Description $description
-    Start-Service $name
+    $nssm = Join-Path -Path $nssmPath -ChildPath 'nssm.exe'
+    & $nssm install $name $executable $arguments
+    $null = & $nssm set $name Description $description
+    Start-Service $name
 
 }
 Install-Service -Name corp-webapi -Description 'PolarisWebAPI' -Executable Powershell.exe -Arguments '-ExecutionPolicy Bypass -Command C:\WebApi\mainpol.ps1'
